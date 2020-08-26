ec3d1eb5c3f6531e1be01fee4f5dc6191a1b9f09
Jessamine Brown <brownjessa11@gmail.com>
2020-08-26 14:05:51 -0500

    connect to connection js and return array to stri

---
+++ config/orm.js
@@ -0,0 +1,8 @@
+var connection = require("./connection.js");
+function printQuestionMarks(num) {
+  var arr = [];
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
