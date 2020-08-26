9cc76b450a6a61a1289382d8b4203a7e138e3ebb
Jessamine Brown <brownjessa11@gmail.com>
2020-08-26 13:52:15 -0500

    put route

--- models/burger.js
+++ models/burger.js
@@ -16,3 +16,9 @@ router.post("/burgers/create", function (req, res) {
     res.redirect("/");
   });
 });
+router.put("/burgers/:id", function (req, res) {
+  burger.update(req.params.id, function (result) {
+    console.log(result);
+    res.sendStatus(200);
+  });
+});
